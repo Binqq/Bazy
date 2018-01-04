8049feda8d1740ea607973ae2e4cab847e3cf8a2
Binqq <m-michal-b@wp.pl>
2018-01-05 00:21:04 +0100

    Poczatek
    Taki wstep

---
+++ BazyDanych/BazyDanych/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BazyDanych
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(82, 39);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(171, 22);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(82, 110);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(171, 22);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(22, 42);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(43, 17);
+            this.labelLogin.TabIndex = 2;
+            this.labelLogin.Text = "Login";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(7, 113);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(69, 17);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Password";
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Location = new System.Drawing.Point(248, 198);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.buttonLogIn.Size = new System.Drawing.Size(75, 30);
+            this.buttonLogIn.TabIndex = 4;
+            this.buttonLogIn.Text = "Log in";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Click += new System.EventHandler(this.buttonLogIn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(349, 253);
+            this.Controls.Add(this.buttonLogIn);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Name = "Form1";
+            this.Text = "Bank Wróble&Bojzan SA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonLogIn;
+    }
+}
+
---
+++ BazyDanych/BazyDanych/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace BazyDanych
+{
+    public partial class Form1 : Form
+    {
+        private bool Porownaj(string string1, string string2)
+        {
+            return String.Compare(string1, string2, true, System.Globalization.CultureInfo.InvariantCulture) == 0 ? true : false;
+        }
+        private void PrintRows(DataSet dataSet)
+        {
+            // For each table in the DataSet, print the row values.
+            foreach (DataTable table in dataSet.Tables)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    foreach (DataColumn column in table.Columns)
+                    {
+                        MessageBox.Show(row[column].ToString());
+                    }
+                }
+            }
+        }
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void buttonLogIn_Click(object sender, EventArgs e)
+        {
+            string login;
+            string password;
+            login = textBoxLogin.Text;
+            password = textBoxPassword.Text;
+            string connetionString = null;
+
+            MySqlConnection cnn;
+            connetionString = "server=localhost;database=bank;uid=root;pwd=Bd123456789#";
+            cnn = new MySqlConnection(connetionString);
+            cnn.Open();
+            try
+            {
+
+                string query = "SELECT Login, Passwords FROM customers WHERE Login Like " + login + " AND Passwords LIKE '" + password + "'";
+                MySqlDataAdapter rdr = new MySqlDataAdapter(query, cnn);
+                rdr.SelectCommand = new MySqlCommand(query, cnn);
+                DataSet data = new DataSet();
+                rdr.Fill(data);
+                if (data != null)
+                {
+                    MessageBox.Show("Zalogowano poprawnie");
+
+                    (new FormBank()).ShowDialog();
+
+
+
+                }
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
+
