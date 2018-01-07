3b5ade0fc4d9dd14a59c58393131c01e33ebb733
Binqq <m-michal-b@wp.pl>
2018-01-07 17:24:13 +0100

    a

---
+++ BazyDanych/BazyDanych/CreateCont.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace BazyDanych
+{
+    partial class CreateCont
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.labelAdres = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelMobile = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelPass = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.checkBoxCompanyAccount = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(162, 25);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(33, 17);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Imię";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(30, 161);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(30, 214);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(299, 27);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(299, 64);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 7;
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(162, 64);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(67, 17);
+            this.labelSurname.TabIndex = 8;
+            this.labelSurname.Text = "Nazwisko";
+            // 
+            // labelAdres
+            // 
+            this.labelAdres.AutoSize = true;
+            this.labelAdres.Location = new System.Drawing.Point(162, 119);
+            this.labelAdres.Name = "labelAdres";
+            this.labelAdres.Size = new System.Drawing.Size(45, 17);
+            this.labelAdres.TabIndex = 9;
+            this.labelAdres.Text = "Adres";
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(162, 164);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(28, 17);
+            this.labelPhone.TabIndex = 10;
+            this.labelPhone.Text = "Tel";
+            // 
+            // labelMobile
+            // 
+            this.labelMobile.AutoSize = true;
+            this.labelMobile.Location = new System.Drawing.Point(162, 217);
+            this.labelMobile.Name = "labelMobile";
+            this.labelMobile.Size = new System.Drawing.Size(101, 17);
+            this.labelMobile.TabIndex = 11;
+            this.labelMobile.Text = "tel. komórkowy";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(443, 27);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(44, 17);
+            this.labelPassword.TabIndex = 12;
+            this.labelPassword.Text = "Hasło";
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Location = new System.Drawing.Point(443, 67);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(43, 17);
+            this.labelPass.TabIndex = 13;
+            this.labelPass.Text = "Login";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 70);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Stwórz konto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(439, 234);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 15;
+            this.buttonBack.Text = "Cofnij";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCompanyAccount
+            // 
+            this.checkBoxCompanyAccount.AutoSize = true;
+            this.checkBoxCompanyAccount.Location = new System.Drawing.Point(301, 115);
+            this.checkBoxCompanyAccount.Name = "checkBoxCompanyAccount";
+            this.checkBoxCompanyAccount.Size = new System.Drawing.Size(119, 21);
+            this.checkBoxCompanyAccount.TabIndex = 16;
+            this.checkBoxCompanyAccount.Text = "Konto firmowe";
+            this.checkBoxCompanyAccount.UseVisualStyleBackColor = true;
+            // 
+            // CreateCont
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 269);
+            this.Controls.Add(this.checkBoxCompanyAccount);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelPass);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelMobile);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelAdres);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelName);
+            this.Name = "CreateCont";
+            this.Text = "CreateCont";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.Label labelAdres;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelMobile;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.CheckBox checkBoxCompanyAccount;
+    }
+}
--- BazyDanych/BazyDanych/Credit.Designer.cs
+++ BazyDanych/BazyDanych/Credit.Designer.cs
@@ -29,11 +29,17 @@
         private void InitializeComponent()
         {
             this.buttonBack = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelPesel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonBack
             // 
-            this.buttonBack.Location = new System.Drawing.Point(214, 213);
+            this.buttonBack.Location = new System.Drawing.Point(425, 250);
             this.buttonBack.Name = "buttonBack";
             this.buttonBack.Size = new System.Drawing.Size(115, 28);
             this.buttonBack.TabIndex = 0;
@@ -41,20 +47,81 @@
             this.buttonBack.UseVisualStyleBackColor = true;
             this.buttonBack.Click += new System.EventHandler(this.buttonLogOut_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(151, 32);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(78, 17);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Imie klienta";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(151, 71);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(112, 17);
+            this.labelSurname.TabIndex = 4;
+            this.labelSurname.Text = "Nazwisko klienta";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // labelPesel
+            // 
+            this.labelPesel.AutoSize = true;
+            this.labelPesel.Location = new System.Drawing.Point(151, 108);
+            this.labelPesel.Name = "labelPesel";
+            this.labelPesel.Size = new System.Drawing.Size(52, 17);
+            this.labelPesel.TabIndex = 6;
+            this.labelPesel.Text = "PESEL";
+            // 
             // Credit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(356, 253);
+            this.ClientSize = new System.Drawing.Size(562, 302);
+            this.Controls.Add(this.labelPesel);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonBack);
             this.Name = "Credit";
             this.Text = "Credit";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelPesel;
     }
 }
