ae7e4d5e860835409172e0d140ba05c1d9d4f99a
Binqq <m-michal-b@wp.pl>
2018-01-08 22:48:02 +0100

    Natępne

--- BazyDanych/BazyDanych/Form2.Designer.cs
+++ BazyDanych/BazyDanych/Form2.Designer.cs
@@ -29,26 +29,63 @@
         private void InitializeComponent()
         {
             this.buttonLogOut = new System.Windows.Forms.Button();
+            this.buttonBalance = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonHistory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogOut
             // 
-            this.buttonLogOut.Location = new System.Drawing.Point(286, 206);
+            this.buttonLogOut.Location = new System.Drawing.Point(435, 361);
             this.buttonLogOut.Name = "buttonLogOut";
-            this.buttonLogOut.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogOut.Size = new System.Drawing.Size(88, 78);
             this.buttonLogOut.TabIndex = 0;
             this.buttonLogOut.Text = "Wyloguj";
             this.buttonLogOut.UseVisualStyleBackColor = true;
             this.buttonLogOut.Click += new System.EventHandler(this.buttonLogOut_Click);
             // 
+            // buttonBalance
+            // 
+            this.buttonBalance.Location = new System.Drawing.Point(12, 35);
+            this.buttonBalance.Name = "buttonBalance";
+            this.buttonBalance.Size = new System.Drawing.Size(99, 60);
+            this.buttonBalance.TabIndex = 1;
+            this.buttonBalance.Text = "Stan Konta";
+            this.buttonBalance.UseVisualStyleBackColor = true;
+            this.buttonBalance.Click += new System.EventHandler(this.buttonBalance_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 145);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(511, 183);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // buttonHistory
+            // 
+            this.buttonHistory.Location = new System.Drawing.Point(147, 35);
+            this.buttonHistory.Name = "buttonHistory";
+            this.buttonHistory.Size = new System.Drawing.Size(118, 60);
+            this.buttonHistory.TabIndex = 3;
+            this.buttonHistory.Text = "Historia Transakcji";
+            this.buttonHistory.UseVisualStyleBackColor = true;
+            this.buttonHistory.Click += new System.EventHandler(this.buttonHistory_Click);
+            // 
             // FormBank
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 253);
+            this.ClientSize = new System.Drawing.Size(535, 451);
+            this.Controls.Add(this.buttonHistory);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonBalance);
             this.Controls.Add(this.buttonLogOut);
             this.Name = "FormBank";
             this.Text = "Klient";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +93,8 @@
         #endregion
 
         private System.Windows.Forms.Button buttonLogOut;
+        private System.Windows.Forms.Button buttonBalance;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonHistory;
     }
 }
--- BazyDanych/BazyDanych/Form2.cs
+++ BazyDanych/BazyDanych/Form2.cs
@@ -30,5 +30,35 @@ namespace BazyDanych
             
 
         }
+
+        private void buttonBalance_Click(object sender, EventArgs e)
+        {
+            cnn.Close();
+           
+            cnn.Open();
+          string  query2 = "SELECT Balacne FROM accounts   JOIN customers ON Customer_id=customers.Customers_id AND customers.Login='" + data + "'";
+            using (MySqlCommand cmdSel = new MySqlCommand(query2, cnn))
+            {
+                DataTable dt = new DataTable();
+                MySqlDataAdapter rdr = new MySqlDataAdapter(cmdSel);
+                rdr.Fill(dt);
+                dataGridView1.DataSource = dt.DefaultView;
+            }
+        }
+
+        private void buttonHistory_Click(object sender, EventArgs e)
+        {
+            cnn.Close();
+
+            cnn.Open();
+            string query2 = "SELECT Transaction_id as 'Id transakcji', Data_Type as 'Data', Login as 'Login' , Employee_id as 'numer pracownika'  FROM transactions   JOIN accounts ON transactions.Account_id=accounts.Account_id JOIN customers ON accounts.Customer_id=customers.Customers_id AND customers.Login = '"+data+"'";
+            using (MySqlCommand cmdSel = new MySqlCommand(query2, cnn))
+            {
+                DataTable dt = new DataTable();
+                MySqlDataAdapter rdr = new MySqlDataAdapter(cmdSel);
+                rdr.Fill(dt);
+                dataGridView1.DataSource = dt.DefaultView;
+            }
+        }
     }
 }
